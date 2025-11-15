7a7e9394bc00356afb8a11c9720eff08061d039a
arifalmas <freelancerarifar@gmail.com>
2025-11-15 11:10:51 +0600

    book a call updated

--- src/app/book-a-call/BookACallClient.tsx
+++ src/app/book-a-call/BookACallClient.tsx
@@ -16,7 +16,7 @@ export default function BookACallClient() {
                 <>
                         <div className="min-h-screen flex flex-col items-center justify-center px-4 py-12 text-white font-[Poppins] ">
                                 <h1 className="text-3xl md:text-4xl font-semibold mb-4 text-white text-center">
-                                        Book a 15-Minute Call with Zarin Rafah
+                                        Book a 30-Minute Call with Zarin Rafah
                                 </h1>
                                 <p className="text-gray-400 mb-8 text-center max-w-xl">
                                         Let's discuss your web project, ideas, or collaboration plans. Pick a
@@ -25,8 +25,8 @@ export default function BookACallClient() {
 
                                 <div className="w-full max-w-5xl h-[600px] z-20 bg-[#1E1E1E] rounded-xl overflow-hidden border border-gray-700">
                                         <Cal
-                                                namespace="15min"
-                                                calLink="zarinrafah/15min"
+                                                namespace="30min"
+                                                calLink="zarinrafah/30min"
                                                 style={{
                                                         width: "100%",
                                                         height: "100%",
